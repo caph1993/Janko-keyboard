fbfad4d0dad94a836d06fe98417bb4e55b4159d5
Carlos Pinzón <caph1993@gmail.com>
2024-12-28 15:06:40 +0100

    Update jankoScript.js

--- jankoScript.js
+++ jankoScript.js
@@ -1,36 +1,45 @@
 
 
-function soundHandler(music){
-		const playSound = new Audio(music)
-		playSound.play()
-		playSound.currentTime= 0
-	}
+const store = {};
 
-const notes = document.querySelectorAll(".key");
-[...notes].forEach(item=> {
-  item.addEventListener('click', (e) => {
-  	e.stopPropagation()
-  	if (!e.target.id ) return
-  	let makeSong = `notes2/${e.target.id}.wav`
-  	soundHandler(makeSong)
-	 })
-   })
-
-
-function soundPlay(e){
- 		const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
- 		const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
- 		if(!audio)return; //stop the function from running all together
- 		audio.currentTime = 0;
- 		audio.play();
- 		key.classList.add('playing');
-
- 	}
-function removeTransition(e) {
-	if (e.propertyName !== 'transform') return; // skip it if it's not a transform
-	this.classList.remove('playing')
+function play(code) {
+	const { key, audio, promise } = store[code];
+	if (!audio || !key) return; //stop the function from running all together
+	audio.currentTime = 0;
+	audio.play();
+	if (promise) clearTimeout(promise);
+	store[code].promise = setTimeout(() => key.classList.remove('playing'), 100);
+	key.classList.add('playing');
 }
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-document.addEventListener('keydown',  soundPlay);
-document.addEventListener('onClick',  soundPlay);
+
+document.addEventListener('keydown', (e) => play(e.code));
+
+
+const shortCodes = {
+	'Minus': '-', 'Equal': '=', 'BracketLeft': '[', 'BracketRight': ']', 'Semicolon': ';', 'Quote': '\'', 'Backslash': '\\', 'Comma': ',', 'Period': '.', 'Slash': '/', 'IntlBackslash': '\\', 'ContextMenu': '☰', 'PrintScreen': 'PrtSc', 'ScrollLock': 'ScrLk', 'Pause': 'Pause', 'Backquote': '`'
+};
+
+
+[...document.querySelectorAll(".key")].forEach(key => {
+	const code = key.getAttribute('data-code');
+	if (!code) return;
+
+	let short = `${code}`;
+	if (short.slice(0, 3) == 'Key') short = `(${short.slice(3)})`;
+	else if (short.slice(0, 5) == 'Digit') short = `(${short.slice(5)})`;
+	else short = `(${shortCodes[short] || short})`;
+
+	const p = document.createElement('p');
+	p.innerHTML = `${key.id[0]}<sup>${key.id.slice(1).replace('S', '#')}<sup></br>${short}</sup></sup>`;
+	key.appendChild(p);
+
+	const audio = document.createElement('audio');
+	audio.src = `notes/${key.id}.wav`;
+	key.appendChild(audio);
+
+	store[code] = { key, audio };
+	key.addEventListener('click', (e) => {
+		e.stopPropagation();
+		play(code);
+	});
+})
